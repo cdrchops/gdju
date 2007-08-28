89ab39e85a080f19be62a3bdbfaf041009dc562f
standtrooper <standtrooper@ed37e578-3230-0410-ad45-b33a795cc63f>
2007-08-28 13:55:54 +0000

    Failed commit: Default (3)

---
+++ src/groovy/app/test/com/gerbildrop/groovy/ant/AntTest.java
@@ -0,0 +1,15 @@
+package com.gerbildrop.groovy.ant;
+
+/**
+ * $ID:$
+ * $COPYRIGHT:$
+ *
+ * @author torr
+ * @since Aug 20, 2007 - 3:00:55 PM
+ */
+
+public class AntTest {
+    public static void main(String[] args) {
+        new AntBuild("squad66");
+    }
+}
